0020ea44ab2440b868f585f99d9a11f0c284546e
Dikachi_Dev <onyex11@gmail.com>
2024-01-04 10:08:05 +0100

    seller update added

---
+++ Workhub.Application/Authentication/Seller/Commands/UpdateSellerCommand.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workhub.Application.Authentication.Seller.Common;
+
+namespace Workhub.Application.Authentication.Seller.Commands;
+
+public record UpdateSellerCommand(string FirstName,
+string LastName,
+string Email,
+string PhoneNumber,
+string Country,
+string State,
+string Address,
+string Occupation,
+string Experience) : IRequest<ErrorOr<SellerAuthResult>>;
+
