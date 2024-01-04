524bc869a7ea6a4bb6de200deb42b6bdc47b3e8d
Dikachi_Dev <onyex11@gmail.com>
2024-01-04 09:38:51 +0100

    added register handler

---
+++ Workhub.Application/Authentication/Seller/Commands/CreateSellerCommand.cs
@@ -0,0 +1,25 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workhub.Application.Authentication.Seller.Common;
+using Workhub.Domain.Entities;
+
+namespace Workhub.Application.Authentication.Seller.Commands;
+
+
+public record CreateSellerCommand(
+string FirstName,
+string LastName,
+string Email,
+string PhoneNumber,
+string Password,
+string Country,
+string State,
+string Address,
+string Occupation,
+string Gender,
+string Experience) : IRequest<ErrorOr<SellerAuthResult>>;
---
+++ Workhub.Application/Authentication/Seller/Query/GetSellerQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workhub.Domain.Entities;
+
+namespace Workhub.Application.Authentication.Seller.Query;
+public class GetSellerQuery : IRequest<IEnumerable<SellerProfile>>
+{
+
+}
