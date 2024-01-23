384d0e5255e677c4b090e8511493e437b6146a12
Dikachi-Dev <onyex11@gmail.com>
2024-01-23 20:51:23 +0100

    Added Serilog

---
+++ Workhub.Application/Interfaces/GlobalLogger/ISeriLogger.cs
@@ -0,0 +1,8 @@
+﻿namespace Workhub.Application.Interfaces.Logger;
+
+public interface ISeriLogger
+{
+    void LogInformation(string username, DateTime dateTime);
+    void LogInError(string username, DateTime dateTime, string message);
+    void LogExceptions(string message, DateTime dateTime);
+}
---
+++ Workhub.Infrastructure/GlobalLogger/Logger.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+using Workhub.Application.Interfaces.Logger;
+
+namespace Workhub.Infrastructure.GlobalLogger
+{
+
+    internal class Logger : ISeriLogger
+    {
+        private readonly ILogger logger;
+
+        public Logger(ILogger logger)
+        {
+            logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        public void LogExceptions(string message, DateTime dateTime)
+        {
+            logger.Fatal(message, dateTime);
+        }
+
+        public void LogInError(string username, DateTime dateTime, string message)
+        {
+            logger.Error($"Login by {username} on {dateTime} Failed With Message {message}");
+        }
+
+        public void LogInformation(string username, DateTime dateTime)
+        {
+            logger.Information($"Login by {username}  on {dateTime} SuccessFul");
+        }
+
+
+    }
+}
