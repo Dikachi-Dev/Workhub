1eaa9f4062127aa0fc8cff721da5f0fa9d2e51b1
Dikachi-Dev <onyex11@gmail.com>
2024-02-01 08:49:06 +0100

    started writing tests for the methods

--- Workhub.Domain/Entities/JobRequest.cs
+++ Workhub.Domain/Entities/JobRequest.cs
@@ -2,9 +2,9 @@
 {
     public class JobRequest : BaseEntity
     {
-        public string? BuyerName { get; set; }
-        public string? SellerName { get; set; }
-        public string? SellerId { get; set; }
-        public string? BuyerId { get; set; }
+        public string BuyerName { get; set; } = string.Empty;
+        public string SellerName { get; set; } = string.Empty;
+        public string SellerId { get; set; } = string.Empty;
+        public string BuyerId { get; set; } = string.Empty;
     }
 }
